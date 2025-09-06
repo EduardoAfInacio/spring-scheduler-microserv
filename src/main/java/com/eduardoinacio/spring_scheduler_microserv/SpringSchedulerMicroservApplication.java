986393bdb6fee1900f30df445a1cd5c4fc481bbe
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-06 15:04:54 -0300

    first commit

---
+++ src/main/java/com/eduardoinacio/spring_scheduler_microserv/SpringSchedulerMicroservApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoinacio.spring_scheduler_microserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulerMicroservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchedulerMicroservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardoinacio/spring_scheduler_microserv/SpringSchedulerMicroservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoinacio.spring_scheduler_microserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedulerMicroservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
