cb34dfcf95fe7d019cfc5419b4c03b57e0e1f98b
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-08 14:26:00 -0300

    Creating findByStatusInAndSendDateBefore in NotificationRepository.

--- src/main/java/com/eduardoinacio/spring_scheduler_microserv/repository/NotificationRepository.java
+++ src/main/java/com/eduardoinacio/spring_scheduler_microserv/repository/NotificationRepository.java
@@ -1,9 +1,15 @@
 package com.eduardoinacio.spring_scheduler_microserv.repository;
 
 import com.eduardoinacio.spring_scheduler_microserv.entity.Notification;
+import com.eduardoinacio.spring_scheduler_microserv.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByStatusInAndSendDateBefore(List<Status> statuses, LocalDateTime sendDateBefore);
 }
