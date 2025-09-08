4ea7d6f7903a08c8fcccf82a8d7a0b9d9fb5baeb
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-08 12:54:37 -0300

    Adding validation to ScheduleNotificationDTO.

--- src/main/java/com/eduardoinacio/spring_scheduler_microserv/controller/dto/ScheduleNotificationDTO.java
+++ src/main/java/com/eduardoinacio/spring_scheduler_microserv/controller/dto/ScheduleNotificationDTO.java
@@ -3,12 +3,22 @@ package com.eduardoinacio.spring_scheduler_microserv.controller.dto;
 import com.eduardoinacio.spring_scheduler_microserv.entity.Enum.ChannelValues;
 import com.eduardoinacio.spring_scheduler_microserv.entity.Enum.StatusValues;
 import com.eduardoinacio.spring_scheduler_microserv.entity.Notification;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 
 import java.time.LocalDateTime;
 
-public record ScheduleNotificationDTO(LocalDateTime dateTime,
+public record ScheduleNotificationDTO(@NotNull(message = "Datetime cannot be null")
+                                      @Size(min = 3, max = 255, message = "Datetime must be between 3 and 255 characters")
+                                      LocalDateTime dateTime,
+                                      @NotNull(message = "Destination cannot be null")
+                                      @Size(min = 3, max = 255, message = "Destination must be between 3 and 255 characters")
                                       String destination,
+                                      @NotNull(message = "Message cannot be null")
+                                      @Size(min = 1, max = 1000, message = "Message must be between 1 and 1000 characters")
                                       String message,
+                                      @NotNull(message = "Channel cannot be null")
                                       ChannelValues channel) {
 
     public Notification toNotification(){
