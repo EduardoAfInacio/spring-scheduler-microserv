66685980cd63d882863637cfb2806f46ebe2bd02
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-08 12:55:57 -0300

    Adding @Valid annotation to controller.

--- src/main/java/com/eduardoinacio/spring_scheduler_microserv/controller/NotificationController.java
+++ src/main/java/com/eduardoinacio/spring_scheduler_microserv/controller/NotificationController.java
@@ -3,7 +3,7 @@ package com.eduardoinacio.spring_scheduler_microserv.controller;
 import com.eduardoinacio.spring_scheduler_microserv.controller.dto.ScheduleNotificationDTO;
 import com.eduardoinacio.spring_scheduler_microserv.entity.Notification;
 import com.eduardoinacio.spring_scheduler_microserv.service.NotificationService;
-import org.apache.coyote.Response;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +17,7 @@ public class NotificationController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> registerNotification(@RequestBody ScheduleNotificationDTO dto) {
+    public ResponseEntity<Void> registerNotification(@RequestBody @Valid ScheduleNotificationDTO dto) {
         notificationService.scheduleNotification(dto);
         return ResponseEntity.accepted().build();
     }
