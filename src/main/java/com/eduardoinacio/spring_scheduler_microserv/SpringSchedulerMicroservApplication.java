adfa71f4ea2907661ee35f9a63d0c08ff32f686b
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-08 13:47:16 -0300

    Adding @EnableScheduling and @EnableAsync annotations to project main.

--- src/main/java/com/eduardoinacio/spring_scheduler_microserv/SpringSchedulerMicroservApplication.java
+++ src/main/java/com/eduardoinacio/spring_scheduler_microserv/SpringSchedulerMicroservApplication.java
@@ -2,8 +2,12 @@ package com.eduardoinacio.spring_scheduler_microserv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class SpringSchedulerMicroservApplication {
 
 	public static void main(String[] args) {
