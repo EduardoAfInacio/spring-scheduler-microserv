533e38afbccca34ea92634fcd822705a672c66e0
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-08 12:46:10 -0300

    Implemented getNotification endpoind in NotificationController.

--- src/main/java/com/eduardoinacio/spring_scheduler_microserv/controller/NotificationController.java
+++ src/main/java/com/eduardoinacio/spring_scheduler_microserv/controller/NotificationController.java
@@ -1,12 +1,11 @@
 package com.eduardoinacio.spring_scheduler_microserv.controller;
 
 import com.eduardoinacio.spring_scheduler_microserv.controller.dto.ScheduleNotificationDTO;
+import com.eduardoinacio.spring_scheduler_microserv.entity.Notification;
 import com.eduardoinacio.spring_scheduler_microserv.service.NotificationService;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/notifications")
@@ -18,8 +17,14 @@ public class NotificationController {
     }
 
     @PostMapping
-    public ResponseEntity<?> registerNotification(@RequestBody ScheduleNotificationDTO dto) {
+    public ResponseEntity<Void> registerNotification(@RequestBody ScheduleNotificationDTO dto) {
         notificationService.scheduleNotification(dto);
         return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/{notificationId}")
+    public ResponseEntity<Notification> getNotification(@PathVariable("notificationId") Long notificationId){
+        Notification notification = notificationService.getNotification(notificationId);
+        return ResponseEntity.ok(notification);
     }
 }
