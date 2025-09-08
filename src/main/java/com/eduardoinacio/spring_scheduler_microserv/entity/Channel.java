b68b7375f2be541c8571a3b5c9a8dc5ee7390274
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-08 10:19:23 -0300

    Adding all args constructor to Channel.

--- src/main/java/com/eduardoinacio/spring_scheduler_microserv/entity/Channel.java
+++ src/main/java/com/eduardoinacio/spring_scheduler_microserv/entity/Channel.java
@@ -3,11 +3,13 @@ package com.eduardoinacio.spring_scheduler_microserv.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "tb_channels")
 public class Channel {
